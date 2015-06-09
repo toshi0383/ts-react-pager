d71ae84b69480509396c88b75856ccb90f48592f
toshi0383 <t.suzuki326@gmail.com>
2015-06-09 23:01:33 +0900

    add test

--- test/index.js
+++ test/index.js
@@ -5,14 +5,19 @@ var should = require('should'),
 function good() {
   console.log("good")
 }
-describe('#Pager', function() {
-  it('should be something', function() {
-    var o = {
-      dataLength:data.length,
-      handler: handler,
-      pageSize: 5,
-      currentPage: 1
-    }
-    console.log(Pager(o))
+var o = {
+  dataLength:data.length,
+  handler: handler,
+  pageSize: 1,
+  currentPage: 1
+}
+var p = Pager(o)
+describe('#Pager.type', function() {
+  it('should be ul', function() {
+    p.type.should.equal('ul')
+  });
+  it('should be ul', function() {
+    var children = p._store.props.children
+    children.length.should.equal(5)
   });
 })
