93cde9ce84fe7b9bd0a7275971bf8502d34fda9c
toshi0383 <t.suzuki326@gmail.com>
2015-06-10 05:14:03 +0900

    [mod] add test which fails

--- test/index.js
+++ test/index.js
@@ -3,20 +3,31 @@ var should = require('should'),
     data = ["apple", "banana", "grape"],
     handler = function() {this.good},
     React = require('react')
-
+function getObject(data) {
+  var o = {
+    dataLength:data.length,
+    handler: handler,
+    pageSize: 1,
+    currentPage: 1
+  }
+  return o
+}
 function good() {
   console.log("good")
 }
-var o = {
-  dataLength:data.length,
-  handler: handler,
-  pageSize: 1,
-  currentPage: 1
-}
-var p = Pager(o)
 
+var p = Pager(getObject(data))
 describe('#Pager', function() {
   it('should have 3 elements', function() {
     p.length.should.equal(5)
+  });
+})
+
+var moreData = data.concat(["chocolate", "berry", "pine", "pumpkin", "carrot"])
+var p2 = Pager(getObject(moreData))
+console.log(p2)
+describe('#Pager', function() {
+  it('should have 9 elements when has more than 8 pages.', function() {
+    p2.length.should.equal(9)
   });
 })
