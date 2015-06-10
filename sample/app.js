100343fc689b35ff4a8b8611b6ced813d23c65fa
toshi0383 <t.suzuki326@gmail.com>
2015-06-10 11:52:19 +0900

    update sample

--- sample/app.js
+++ sample/app.js
@@ -19881,23 +19881,25 @@ process.umask = function() { return 0; };
 },{}],158:[function(require,module,exports){
 var Pager = require('../bin/pager').Pager
 var React = require('react')
-var data = ["apple", "banana", "grape"],
-    handler = function() {console.log("test")}
+var data = ["apple", "banana", "grape"]
 data = data.concat(["chocolate", "berry", "pine", "pumpkin", "carrot"])
 var App = React.createClass({displayName: "App",
   getInitialState: function() {
-    return {data:data}
+    return {data:data, currentPage:1}
+  },
+  handlePaging: function(pageNum) {
+    this.setState({data:this.state.data, currentPage:pageNum})
   },
   buttonHandler: function() {
     var d = this.state.data.slice(0, this.state.data.length - 1)
-    this.setState({data:d})
+    this.setState({data:d, currentPage:this.state.currentPage})
   },
   render: function() {
     var o = {
       dataLength:this.state.data.length,
-      handler: handler,
+      handler: this.handlePaging,
       pageSize: 1,
-      currentPage: 1
+      currentPage: this.state.currentPage
     }
     return (
       React.createElement("div", null, 
--- sample/app.jsx
+++ sample/app.jsx
@@ -1,22 +1,24 @@
 var Pager = require('../bin/pager').Pager
 var React = require('react')
-var data = ["apple", "banana", "grape"],
-    handler = function() {console.log("test")}
+var data = ["apple", "banana", "grape"]
 data = data.concat(["chocolate", "berry", "pine", "pumpkin", "carrot"])
 var App = React.createClass({
   getInitialState: function() {
-    return {data:data}
+    return {data:data, currentPage:1}
+  },
+  handlePaging: function(pageNum) {
+    this.setState({data:this.state.data, currentPage:pageNum})
   },
   buttonHandler: function() {
     var d = this.state.data.slice(0, this.state.data.length - 1)
-    this.setState({data:d})
+    this.setState({data:d, currentPage:this.state.currentPage})
   },
   render: function() {
     var o = {
       dataLength:this.state.data.length,
-      handler: handler,
+      handler: this.handlePaging,
       pageSize: 1,
-      currentPage: 1
+      currentPage: this.state.currentPage
     }
     return (
       <div>
